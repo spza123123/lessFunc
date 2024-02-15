8282f2be4062581d3cfcfa740a2acd5f319a2635
Zakhar <info@zspirin.ru>
2024-02-15 14:16:55 +0300

    Task1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿double CalcuulateFormula(int a, int b, int c, int d)
+{
+    double numerator = a * b;
+    int denumerator = c + d;
+    double result = numerator / denumerator;
+    return result;
+}
+
+
+
+double result = CalcuulateFormula(1, 2, 3, 4);
+System.Console.WriteLine(result);
